a319041e048482087162a4a3194feb97ba77b26a
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-01-09 13:49:22 -0800

    Scriptify test archiving

---
+++ project/stage/archive_tests.sh
@@ -0,0 +1,11 @@
+target_test_dir=$1
+
+tests=$(find $target_test_dir -type f -name "*.ul")
+for filename in $tests
+do
+    # hides ".ul" extension, so that run_ul_tests script ignores
+    mv $filename "$filename.archived"
+done
+
+numTests="$(echo $tests | wc -w)"
+echo "\nArchived $numTests tests"
