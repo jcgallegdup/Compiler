c0491f9894c352d9da4fe7523cc68ac52c539df2
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-01-09 19:11:11 -0800

    Generalize script for generating tests out of tests

--- project/stage/copy_and_append_statements.sh
+++
@@ -1,5 +0,0 @@
-path_to_original_file=$1
-path_to_new_file_w_name=$2
-remove=$3
-statements=$4
-touch $path_to_new_file_w_name; cat $path_to_original_file | sed s"/$remove/$statements}/" >> $path_to_new_file_w_name
