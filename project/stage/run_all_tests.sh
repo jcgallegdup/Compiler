f6d3410f47ed00f94ab983370021908823f6f105
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-01-21 00:11:24 -0800

    Write test output to files in untracked directory

--- project/stage/run_all_tests.sh
+++ project/stage/run_all_tests.sh
@@ -2,11 +2,12 @@
 # sed removes the extra frontslash in path
 tests=$(find tests/$1 -type f -name "*.ul" | sed 's/\/\//\//')
 # TODO print seprator between accept and reject tests
-for filename in $tests
+for pathToTest in $tests
 do
     # TODO ensure that Compiler.class exists in current dir
-    echo "* Running $filename"
-    output=$(java Compiler $filename)
+    echo "* Running $pathToTest"
+    testFileName=$(echo $pathToTest | egrep -o "[a-zA-Z_0-9]*\.ul")
+    java Compiler $pathToTest >> "results/$testFileName.out"
     echo $output
     echo "\n\n"
 done
