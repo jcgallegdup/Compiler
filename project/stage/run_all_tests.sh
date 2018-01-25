ccc34bf8990dd9c7c8512cd2a73540c3f8c04093
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-01-25 12:27:52 -0800

    Scriptify regression tests for pretty printing

--- project/stage/run_all_tests.sh
+++ project/stage/run_all_tests.sh
@@ -1,14 +1,22 @@
 # TODO support "-a" and -r" flags to specify "accept" and "reject" tests
+
+testType=$1
+
 # sed removes the extra frontslash in path
-tests=$(find tests/$1 -type f -name "*.ul" | sed 's/\/\//\//')
+tests=$(find tests/$testType -type f -name "*.ul" | sed 's/\/\//\//')
+
 # TODO print seprator between accept and reject tests
 for pathToTest in $tests
 do
     # TODO ensure that Compiler.class exists in current dir
     echo "* Running $pathToTest"
     testFileName=$(echo $pathToTest | egrep -o "[a-zA-Z_0-9]*\.ul")
-    java Compiler $pathToTest >> "results/$testFileName.out"
-    echo $output
+    output=$(java Compiler $pathToTest)
+    if [ -n "$testType" ] && [ "$testType" == "accept" ]
+    then
+        echo "$output" > "results/$testFileName.out"
+        diff "results/$testFileName.out" "expected/$testFileName.out"
+    fi
     echo "\n\n"
 done
 
