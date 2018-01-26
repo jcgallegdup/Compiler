05e8575c51a7358b996d45229073a1459bb16a33
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-01-25 16:06:26 -0800

    Document test script

--- project/stage/run_all_tests.sh
+++ project/stage/run_all_tests.sh
@@ -1,5 +1,5 @@
+# TODO ensure that Compiler.class exists in current dir
 # TODO support "-a" and -r" flags to specify "accept" and "reject" tests
-
 testType=$1
 
 # sed removes the extra frontslash in path
@@ -8,10 +8,15 @@ tests=$(find tests/$testType -type f -name "*.ul" | sed 's/\/\//\//')
 # TODO print seprator between accept and reject tests
 for pathToTest in $tests
 do
-    # TODO ensure that Compiler.class exists in current dir
     echo "* Running $pathToTest"
+
+    # parse out name of file from its path
     testFileName=$(echo $pathToTest | egrep -o "[a-zA-Z_0-9]*\.ul")
+
     output=$(java Compiler $pathToTest)
+
+    # when running the accept tests exclusively,
+    # store the pretty print output and compare with previous output
     if [ -n "$testType" ] && [ "$testType" == "accept" ]
     then
         echo "$output" > "results/$testFileName.out"
